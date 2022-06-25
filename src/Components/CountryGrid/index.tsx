f1cfeefc0b0455a47f4cad2c6d7d8cbf04f3649d
Lucas Rocha <lucasrocha.dv@gmail.com>
2022-06-24 21:15:56 -0300

    Migrate CountryGrid & CountryGridItem to typescript

---
+++ src/Components/CountryGrid/index.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+import { getCountries } from '../../Api/Services/Countries';
+import useCountries from '../../Utils/Hooks/useCountries';
+
+import CountryGridItem from '../CountryGridItem';
+
+import { Container } from './styles';
+
+interface CountryItems {
+  cca2: string;
+  flags: {
+    png: string;
+  };
+  name: {
+    common: string;
+  };
+  population: number;
+  region: string;
+  capital: string;
+}
+
+const CountryGrid: React.FC = () => {
+  const { countries, setCountries } = useCountries();
+
+  useEffect(() => {
+    getCountries().then((response) => {
+      try {
+        localStorage.setItem('countries', JSON.stringify(response.data));
+        return setCountries(response.data);
+      } catch (error) {
+        return error;
+      }
+    });
+  }, []);
+
+  return (
+    <Container>
+      {countries.map((country: CountryItems) => (
+        <Link to={`country/${country.name.common.toLowerCase()}`}>
+          <CountryGridItem
+            key={country.cca2}
+            flag={country.flags.png}
+            name={country.name.common}
+            population={country.population}
+            region={country.capital}
+            capital={country.capital}
+          />
+        </Link>
+      ))}
+    </Container>
+  );
+};
+
+export default CountryGrid;
---
+++ src/Components/CountryGridItem/index.tsx
@@ -0,0 +1,44 @@
+import {
+  Container,
+  Image,
+  InfoBody,
+  InfoList,
+  InfoListItem,
+  Title,
+} from './style';
+
+interface Props {
+  flag: string;
+  name: string;
+  population: number;
+  region: string;
+  capital: string;
+}
+
+const CountryGridItem: React.FC<Props> = ({
+  flag,
+  name,
+  population,
+  region,
+  capital,
+}) => (
+  <Container>
+    <Image src={flag} />
+    <InfoBody>
+      <Title>{name}</Title>
+      <InfoList>
+        <InfoListItem>
+          <b>Population:</b> {population.toLocaleString('en-US')}
+        </InfoListItem>
+        <InfoListItem>
+          <b>Region:</b> {region}
+        </InfoListItem>
+        <InfoListItem>
+          <b>Capital:</b> {capital}
+        </InfoListItem>
+      </InfoList>
+    </InfoBody>
+  </Container>
+);
+
+export default CountryGridItem;
